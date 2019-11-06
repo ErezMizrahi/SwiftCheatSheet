6a05774f2ac2407de3084505e4429e7fff054504
ErezMizrahi <erez8821@gmail.com>
2019-11-06 11:22:21 +0200

    commit

---
+++ SwiftCheatSheet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCheatSheet
+//
+//  Created by Erez Mizrahi on 06/11/2019.
+//  Copyright © 2019 Erez Mizrahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
