5112155a7a949c8d31a39c22047a8f41b317d6c2
Erez Mizrahi <46554664+ErezMizrahi@users.noreply.github.com>
2020-01-15 13:57:16 +0200

    Update Contents.swift

--- SwiftCheatSheet/Algorithms/HexString XOR/XOR.playground/Contents.swift
+++ SwiftCheatSheet/Algorithms/HexString XOR/XOR.playground/Contents.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 
-let hexStrings = "a3:5A:f0:9e:1B:CD:6C:04:1E:DA:8D:A5:6D:B2:E9:46:40:07:63:5a:da:cf".split(separator: ":")
-let param = 0xE3
+let hexStrings = "a3:5A:f0:9e:1B:CD:6C:04:1E:DA:8D:A5:6D:B2:E9:46:40:07:63:5a:da:cf".split(separator: ":") //random 
+let param = 0xE3 //some hex number to xor by
 let numbers = hexStrings.compactMap{Int($0, radix: 16)}
 let result = numbers.map{String(format: "%02x", $0^param)}.joined(separator: ":")
 
